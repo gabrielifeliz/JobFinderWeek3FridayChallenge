d479a15f87312226f7dcff71274ef3d4a7fe8d9c
Gabriel I Feliz <gfeliz3@montgomerycollege.edu>
2018-06-22 15:10:41 -0400

    initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    JobRepository jobRepository;
+
+    @RequestMapping("/")
+    public String listMessages() {
+        return "index";
+    }
+    @GetMapping("/add")
+    public String courseForm(Model model) {
+        model.addAttribute("job", new Job());
+        return "jobform";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Job job, BindingResult result) {
+        if (result.hasErrors()) {
+            return "jobform";
+        }
+        jobRepository.save(job);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/joblist")
+    public String jobList(Model model){
+        model.addAttribute("jobs", jobRepository.findAll());
+        return "joblist";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showJob(@PathVariable("id") long id, Model model) {
+        model.addAttribute("job", jobRepository.findById(id).get());
+        return "showjob";
+    }
+    @RequestMapping("/update/{id}")
+    public String updateJob(@PathVariable("id") long id, Model model) {
+        model.addAttribute("job", jobRepository.findById(id));
+        return "jobform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String deleteJob(@PathVariable("id") long id) {
+        jobRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo/Job.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=1)
+    private String position;
+
+    @NotNull
+    @Size(min=1)
+    private String company;
+
+    @NotNull
+    @Size(min=1)
+    private String requirements;
+
+    @NotNull
+    @Size(min=1)
+    private String desiredSkills;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(String requirements) {
+        this.requirements = requirements;
+    }
+
+    public String getDesiredSkills() {
+        return desiredSkills;
+    }
+
+    public void setDesiredSkills(String desiredSkills) {
+        this.desiredSkills = desiredSkills;
+    }
+}
